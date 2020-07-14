fd3b92efa488de9a9332e513c310cb7221051bdd
yancy <softwarexyy@126.com>
2020-07-15 07:14:41 +0800

    添加跳转到tables页面的日志

--- src/main/java/com/life/controller/TableController.java
+++ src/main/java/com/life/controller/TableController.java
@@ -1,13 +1,18 @@
 package com.life.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TableController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/toTables")
     public String toTables() {
+        logger.info("跳转到表格页");
         return "tables";
     }
 }
